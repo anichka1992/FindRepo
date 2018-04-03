817424a2bc2d2eec0d0d6074ddcd306a20ea08e6
Anna Stasyshena <annastasyshena@Annas-MacBook-Pro.local>
2018-04-02 21:29:16 -0700

    Tests

--- app/src/main/java/com/example/annastasyshena/findrepo/MainActivity.kt
+++ app/src/main/java/com/example/annastasyshena/findrepo/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.annastasyshena.findrepo
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
+
 
         val searchEditText = findViewById<EditText>(R.id.SearchEditText)
         val userRepoEditText = findViewById<EditText>(R.id.userRepoEditText)
@@ -26,5 +31,17 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("username", userRepoEditText.text.toString())
             startActivity(intent)
         }
+    }
+
+    private fun logUser() {
+        // TODO: Use the current user's information
+        // You can call any combination of these three methods
+        Crashlytics.setUserIdentifier("12345")
+        Crashlytics.setUserEmail("user@fabric.io")
+        Crashlytics.setUserName("Test User")
+    }
+
+    fun forceCrash(view: View) {
+        throw RuntimeException("This is a crash")
     }
 }
